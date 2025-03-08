32dea2c835c68530e7e73c46d739d7c95f4f305f
BartelKarlijn <legoreus@gmail.com>
2025-03-08 15:31:54 +0100

    toch eens  opladen

--- src/eprom/save_eprom.h
+++ src/eprom/save_eprom.h
@@ -1,9 +1,19 @@
 #pragma once
 // Complete project details at https://RandomNerdTutorials.com/esp32-save-data-permanently-preferences/
 // The Preferences library in the ESP32 framework provides a way to store and retrieve key-value pairs in non-volatile storage (NVS). 
+#include <Preferences.h>
+#include <PrintAnyFormat.h>
 
 #define NAMESPACE "WIFI"
 
+// Function to Save Wifi data to EEPROM
+// --------------------------------------
+Preferences pref_eeprom;           // to store & read parameters from eprom
+enum WifiParam {
+    SSID,
+    PWD
+  };
+  
 // Function to get data from EEPROM
 String Wifi_get_from_eeprom (WifiParam param) {
   // Open Preferences with WIFI namespace. Each application module, library, etc
@@ -12,31 +22,38 @@ String Wifi_get_from_eeprom (WifiParam param) {
   // Note: Namespace name is limited to 15 chars.
   String returnString;
   if (!pref_eeprom.begin(NAMESPACE, false)) {
-    Serial.println("Failed to open EEPROM");
+    Println("Failed to open EEPROM");
     returnString = "Error";
   }
-  else {
-    // Get the counter value, if the key does not exist, return a default value of 0
-    // Note: Key name is limited to 15 chars.
-    if (param == SSID) {
-      returnString = pref_eeprom.getString("ssid_eeprom", "");
-      Serial.print("Uit eprom uitgelezen waarde voor SSID = ");
-      Serial.println(returnString);
-    } else if (param == PWD) {
-      returnString = pref_eeprom.getString("pwd_eeprom", "");
-      Serial.print("Uit eprom uitgelezen waarde voor PWD = ");
-      Serial.println("*****");
-      returnString = returnString;
-    }
-  
-    // Close Preferences
-    pref_eeprom.end();
+  // Get the counter value, if the key does not exist, return a default value of 0
+  // Note: Key name is limited to 15 chars.
+  if (param == SSID) {
+    returnString = pref_eeprom.getString("ssid_eeprom", "");
+    Print("Uit eprom uitgelezen waarde voor SSID = ");
+    Println(returnString);
+  } else if (param == PWD) {
+    returnString = pref_eeprom.getString("pwd_eeprom", "");
+    Print("Uit eprom uitgelezen waarde voor PWD = ");
+    Println("*****");
+    returnString = returnString;
   }
+  
+  // Close Preferences
+  pref_eeprom.end();
+
   return returnString;
-};
+}
 
 
+// Function to Read Wifi data to EEPROM
+// --------------------------------------
 void Wifi_save_to_eeprom (String wifi_ssid, String wifi_pwd) {
+  // Open Preferences with WIFI namespace in RW-mode
+  if (!pref_eeprom.begin(NAMESPACE, false)) {
+    Serial.println("Failed to open EEPROM");
+    return;
+  }
+
   pref_eeprom.putString("ssid_eeprom", wifi_ssid);
   pref_eeprom.putString("pwd_eeprom" , wifi_pwd);
 
@@ -50,4 +67,7 @@ void Wifi_save_to_eeprom (String wifi_ssid, String wifi_pwd) {
   String pwd_eeprom = pref_eeprom.getString("pwd_eeprom","");
   Print("Uit eprom uitgelezen waarde voor PWD = ");
   Println(pwd_eeprom);
+
+  // Close Preferences
+  pref_eeprom.end();  
 }
