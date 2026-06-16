1979948c16e81f64d177184b1719885f63f8865f
BartelKarlijn <legoreus@gmail.com>
2026-06-16 21:56:24 +0200

    output AP

--- src/wifi/setup_CreateAccessPoint.h
+++ src/wifi/setup_CreateAccessPoint.h
@@ -51,7 +51,7 @@ void setup_CreateAccessPoint(){
   wifi_scan += "</ol>";
 */
   WiFi.softAP(autoConnectAP, "");
-  Serial.println("AP: " + NAME_ACCESSPOINT + " Local IP: http://192.168.4.1/");
+  Serial.println("AP: " + String(NAME_ACCESSPOINT) + " Local IP: http://192.168.4.1/");
    }
 
 }
